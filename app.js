2fee26fc630934039b1e706926d65c6c67c8ea3b
Pushkar Sabe <83827883+pushkarsabe@users.noreply.github.com>
2023-11-03 12:00:35 +0530

    Question 8 - Dynamic Routes Part 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -10,18 +12,13 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const contactRoutes = require('./routes/contact');
-const errorController = require('./controllers/error');
-const successController = require('./controllers/success');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use(contactRoutes);
 
-app.use(successController.getSuccess);
 app.use(errorController.get404);
 
 app.listen(3000);

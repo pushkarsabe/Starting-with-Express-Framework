d55a998cdf6b43cf1657f8d86aff93362b296f83
Pushkar Sabe <83827883+pushkarsabe@users.noreply.github.com>
2023-11-07 17:55:56 +0530

    Question 16- Retrieving data from databases

--- product.js
+++ product.js
@@ -1,24 +1,12 @@
 const fs = require('fs');
-const path = require('path');
 
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
-);
+const db = require('../util/database');
 
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
+const Cart = require('./cart');
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,23 +14,19 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
-    getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
-    });
+    //to protect aginst sql injetcion
+    return db.execute('INSERT INTO products (title,price,description,imageUrl) VALUES (?,?,?,?)', [this.title, this.price, this.description, this.imageUrl]);
   }
 
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
+  static deleteById(id) {
+    return db.execute('DELETE FROM products WHERE products.id = ?', [id]);
   }
 
-  static finById(id, cb) {
-    getProductsFromFile(products=>{
-      const product = products.find(p=> p.id === id);
-      cb(product);
-    })
+  static fetchAll() {
+    return db.execute('select * from products');
+  }
+
+  static findById(id) {
+    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
   }
 };

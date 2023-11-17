e016a7433ec9815b28f7cd38aa9ff8636a3e8f6c
Pushkar Sabe <83827883+pushkarsabe@users.noreply.github.com>
2023-11-17 10:18:27 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -33,8 +35,15 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+//Product will have a FK from user table on userid
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+//Cart will have a FK from user table on userid
+Cart.belongsTo(User);
+//save the connections to CartItem
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
   // .sync({ force: true })
@@ -51,6 +60,9 @@ sequelize
   })
   .then(user => {
     // console.log(user);
+    return user.createCart();
+  })
+  .then((cart) => {
     app.listen(3000);
   })
   .catch(err => {
